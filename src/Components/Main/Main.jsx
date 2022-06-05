0a212c2cf3a77d669ef7f71d5015d80be1e17700
MD ALISHAN ALI <92203534+mdalishanali@users.noreply.github.com>
2022-06-05 15:53:12 +0530

    mainPage

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -3,10 +3,12 @@ import { Container } from '../GlobalComponents/Container'
 import { Overlay } from '../GlobalComponents/Overlay'
 import { MainInfo } from './MainInfo'
 import { Nav } from './Navbar/Nav'
-
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import mainBgOne from "../Images/mainBgOne.png";
 export const Main = () => {
   return (
-    <section>
+    <section css={styles}>
         <Overlay/> 
         <Nav/>
         <Container>
@@ -15,3 +17,22 @@ export const Main = () => {
     </section>
   )
 }
+
+const styles = css`
+  width: 100%;
+  min-height: 100vh;
+  background: url("${mainBgOne}") no-repeat center/cover;
+  display: flex;
+  position: relative;
+  .container{
+    margin: auto;
+    text-align: center;
+    color: #fff;
+      z-index: 2;
+  }
+  @media (max-width:965px){
+    .container{
+      max-width: 90%;
+    }
+  }
+`
--- src/Components/Main/MainInfo.jsx
+++ src/Components/Main/MainInfo.jsx
@@ -1,12 +1,56 @@
 import React from 'react'
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
+import { Button } from '../GlobalComponents/Button';
 export const MainInfo = () => {
   return (
     <div css={styles} className="mainInfo">
-        
+        <h3>Build Up Yours</h3>
+        <h1>Body Shape</h1>
+        <p>Build Your Body and Fitness with Professional Touch</p>    
+        <Button text="JOIN US"/>
     </div>
   )
 }
 const styles = css`
+h3{
+    font-size: 43px;
+    font-weight: 800;
+    line-height: 1;
+    text-transform: uppercase;
+}
+h1{
+    font-size: 150px;
+    font-weight: 900;
+    line-height: 1;
+    margin: 24px 0;
+    text-transform: uppercase;
+}
+p{
+    font-size: 21px;
+}
+.btn{
+    margin: 32px 0 0 0;
+    padding: 15px 50px;
+    &:hover{
+        background: transparent;
+        color: #ff1414;
+
+    }
+}
+
+@media (max-width:640px){
+    h1{
+        font-size: 40px;
+
+    }
+    p{
+        font-size: 18px;
+    }
+}
+@media (min-width:641px) and (max-width:965px){
+    h1{
+        font-size: 80px;
+    }
+}
 `
